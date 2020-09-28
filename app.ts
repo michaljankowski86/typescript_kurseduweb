2eeaa37b5e0af2840c568190992de0414f992617
michal.jankowski <michaljankowski86@gmail.com>
2020-09-28 17:09:41 +0200

    project setup and first examples

--- app.ts
+++ app.ts
@@ -1,2 +1,37 @@
-console.log('eduweb is watching you... 👀')
-async function foo(): Promise<void> {}
+const data: number[] = [1, 2, 3]
+
+// Tuple - rodzaj tablicy: liczbe elementów i typy są dokładnie określone
+let settings: [string, boolean]
+
+// Object
+const user: { name: string; getName(): string } = {
+  name: "Adam",
+  getName() {
+    return this.name
+  },
+}
+
+// Enum - nazwane określone wartości liczbowe / tekstowe
+enum extensions {
+  doc,
+  docx,
+  pdf,
+}
+
+let coffeeSize: "medium" | "large"
+
+// Union type: wykorzystanie jednego lub drugiego typu
+let stringOrNumber: string | number
+
+// utworzenie nowego typu
+type doubleAnything = string | number
+
+function double(value: doubleAnything): number {
+  if (typeof value === "string") {
+    return parseInt(value, 10) * 2
+  }
+
+  return value * 2
+}
+
+// inferencja - Ts sam potrafi domyślić się typu
